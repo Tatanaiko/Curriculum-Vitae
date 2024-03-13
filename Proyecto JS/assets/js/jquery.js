0b2be0dcc5482b4f56bcc9b35c1d29eadeccde5b
Tatanaiko <140436101+Tatanaiko@users.noreply.github.com>
2024-03-13 11:29:47 -0300

    Delete Proyecto JS directory

--- Proyecto JS/assets/js/jquery.js
+++
@@ -1,8 +0,0 @@
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-let profesion = document.getElementById("profesion");
-let sueldo = document.getElementById("sueldo");
-nombre.innerHTML = prompt("Ingrese su nombre")
-apellido.innerHTML = prompt("Ingrese su apellido")
-profesion.innerHTML = prompt("Ingrese su profesion")
-sueldo.innerHTML = prompt("Ingrese su sueldo")
